a00f3e6b2eb2f2b49ad74be754bba22d5d13ab90
vamsikrishna7-github <vamsikrishna.nagidi@gmail.com>
2025-09-24 19:33:28 +0530

    production fix-backend

--- jobmailer/settings.py
+++ jobmailer/settings.py
@@ -32,6 +32,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+    "https://jobmailer-azure.vercel.app",
+]
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Application definition
 
@@ -43,10 +48,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'mailer',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
